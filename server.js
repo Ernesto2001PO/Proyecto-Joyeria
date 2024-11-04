a9ecaccc6d7f5c57163b93a4bf50f61517d8e5fb
ErnestoNur <143664425+ErnestoNur@users.noreply.github.com>
2024-11-04 14:41:48 -0400

    html corriendo en mi servidor - solo corre el html sin el css

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
+require('dotenv').config();
+
+
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
+
+ 
+
 // Middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Servir archivos estáticos
 app.use(express.static('public'));
-
-// Ruta de prueba
-app.get('/', (req, res) => {
-    res.send('Servidor Express funcionando');
-});
 
 // Iniciar servidor
 app.listen(PORT, () => {
